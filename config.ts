a0ad3ecf2d11729e540057b63004371368347a75
praneeth-coder-2005 <savediskofc@gmail.com>
2025-07-06 18:38:50 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 // SITE CONFIG
 export const NEXT_PUBLIC_SITE_NAME =
-  process.env.NEXT_PUBLIC_SITE_NAME || "reelnn";
+  process.env.NEXT_PUBLIC_SITE_NAME || "irandomguy";
 
 export const NEXT_PUBLIC_FOOTER_DESC =
   process.env.NEXT_PUBLIC_FOOTER_DESC ||
@@ -15,7 +15,7 @@ export const NEXT_PUBLIC_WHATSAPP_CONTACT =
 export const NEXT_PUBLIC_INSTAGRAM_CONTACT =
   process.env.NEXT_PUBLIC_INSTAGRAM_CONTACT || "";
 
-export const SITE_SECRET = process.env.SITE_SECRET || "your_secret_key";
+export const SITE_SECRET = process.env.SITE_SECRET || "#5213680099Ac";
 
 export const ENABLE_AUTH_PROTECTION = process.env.ENABLE_AUTH_PROTECTION === 'true';
 
@@ -28,7 +28,7 @@ export const BACKEND_URL = process.env.BACKEND_URL || "http://0.0.0.0:6519";
 // Telegram
 export const NEXT_PUBLIC_TELEGRAM_BOT_NAME =
   process.env.NEXT_PUBLIC_TELEGRAM_BOT_NAME || "reelnnbot";
-export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '12346578:CJNDSccosadckokco';
+export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '7855398605:AAGenTRSqs7t9Yw6zhiolW5f7QSyq-7kkqE';
 
 // Shortener API
 export const SHORTENER_API_URL = process.env.SHORTENER_API_URL || '';
